01d8ed8ed92fe3929bac82d79aa95a83b0d45d0e
Fanaja <manodarisoa@gmail.com>
2025-05-15 04:32:09 +0300

    [A3]test: qrcodestyled functional

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,58 +1,103 @@
 declare module 'react-native-qrcode-styled' {
-  import { ComponentType } from 'react';
-  import { SvgProps } from 'react-native-svg';
+  import { ComponentType, ReactElement } from 'react';
+  import { SvgProps, ImageProps } from 'react-native-svg';
+  import { ColorValue } from 'react-native';
 
-  // Options pour les yeux (inner et outer)
-  interface EyeOptions {
-    outerColor?: string;
-    innerColor?: string;
-    borderWidth?: number;
-    borderColor?: string;
-  }
+  type GradientType = 'linear' | 'radial';
 
-  // Options pour le dégradé
-  interface GradientProps {
-    type: 'linear' | 'radial';
-    colors: string[];
-    start?: { x: number; y: number };
-    end?: { x: number; y: number };
-  }
+  type LinearGradientProps = {
+    colors?: ColorValue[];
+    start?: [number, number];
+    end?: [number, number];
+    locations?: number[];
+  };
 
-  // Définition du logo
-  interface LogoOptions {
-    uri: string; // URI pour l'image du logo
-    width?: number; // Largeur du logo
-    height?: number; // Hauteur du logo
-    backgroundColor?: string; // Couleur de fond du logo
-  }
+  type RadialGradientProps = {
+    colors?: ColorValue[];
+    center?: [number, number];
+    radius?: [number, number];
+    locations?: number[];
+  };
 
-  // Propriétés principales du QR Code
+  type GradientProps = {
+    type?: GradientType;
+    options?: LinearGradientProps | RadialGradientProps;
+  };
+
+  type EyePosition = 'topLeft' | 'topRight' | 'bottomLeft';
+
+  type EyeOptions = {
+    scale?: SvgProps['scale'];
+    rotation?: string | number;
+    borderRadius?: number | number[];
+    color?: ColorValue;
+    gradient?: GradientProps;
+    stroke?: ColorValue;
+    strokeWidth?: number;
+  };
+
+  type AllEyesOptions = { [K in EyePosition]?: EyeOptions };
+
+  type LogoArea = {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+  };
+
+  type LogoOptions = {
+    hidePieces?: boolean;
+    padding?: number;
+    scale?: number;
+    onChange?: (logoArea?: LogoArea) => void;
+  } & ImageProps;
+
+  type RenderCustomPieceItem = ({
+    x,
+    y,
+    pieceSize,
+    qrSize,
+    bitMatrix,
+  }: {
+    x: number;
+    y: number;
+    pieceSize: number;
+    qrSize: number;
+    bitMatrix: number[][];
+  }) => ReactElement | null;
+
   interface QRCodeStyledProps extends SvgProps {
-    data: string | string[]; // Message à encoder dans le QR Code
-    style?: object; // Style personnalisé pour le QR Code
-    padding?: number; // Padding autour du QR code
-    pieceSize?: number; // Taille de chaque élément du QR Code
-    pieceScale?: number; // Échelle de chaque élément du QR Code
-    pieceRotation?: number; // Rotation des éléments du QR Code (en degrés)
-    pieceCornerType?: 'rounded' | 'cut'; // Type des coins des éléments du QR code
-    pieceBorderRadius?: number | number[]; // Rayon de bord des éléments du QR Code
-    pieceStroke?: string; // Couleur de la bordure des éléments
-    pieceStrokeWidth?: number; // Largeur de la bordure des éléments
-    pieceLiquidRadius?: number; // Rayon de l'effet de liquide entre les pièces
-    isPiecesGlued?: boolean; // Si les pièces du QR code sont "collées" entre elles
-    outerEyesOptions?: EyeOptions; // Options pour les yeux extérieurs
-    innerEyesOptions?: EyeOptions; // Options pour les yeux intérieurs
-    color?: string; // Couleur des éléments du QR Code
-    gradient?: GradientProps; // Gradient appliqué au QR Code
-    logo?: LogoOptions; // Configuration du logo
-    backgroundImage?: { uri: string }; // Image de fond pour le QR Code
-    version?: number; // Version du QR Code
-    maskPattern?: number; // Modèle de masque pour le QR Code
-    toSJISFunc?: (data: string) => any; // Fonction de conversion en SJIS
-    errorCorrectionLevel?: 'L' | 'M' | 'Q' | 'H'; // Niveau de correction d'erreur
-    renderCustomPieceItem?: (pieceSize: number, bitMatrix: number[][]) => JSX.Element; // Rendu personnalisé pour chaque élément
-    renderBackground?: (pieceSize: number, bitMatrix: number[][]) => JSX.Element; // Rendu de l'arrière-plan
-    children?: (pieceSize: number, bitMatrix: number[][]) => JSX.Element; // Composants supplémentaires à ajouter
+    data: string | string[];
+    style?: object;
+    padding?: number;
+    pieceSize?: number;
+    pieceScale?: SvgProps['scale'];
+    pieceRotation?: SvgProps['rotation'];
+    pieceCornerType?: 'rounded' | 'cut';
+    pieceBorderRadius?: number | number[];
+    pieceStroke?: ColorValue;
+    pieceStrokeWidth?: number;
+    pieceLiquidRadius?: number;
+    isPiecesGlued?: boolean;
+    outerEyesOptions?: EyeOptions | AllEyesOptions;
+    innerEyesOptions?: EyeOptions | AllEyesOptions;
+    color?: ColorValue;
+    gradient?: GradientProps;
+    logo?: LogoOptions;
+    backgroundImage?: ImageProps;
+    version?: number;
+    maskPattern?: number;
+    toSJISFunc?: (data: string) => any;
+    errorCorrectionLevel?: 'L' | 'M' | 'Q' | 'H';
+    renderCustomPieceItem?: RenderCustomPieceItem;
+    renderBackground?: (
+      pieceSize: number,
+      bitMatrix: number[][]
+    ) => SvgProps['children'];
+    children?: (
+      pieceSize: number,
+      bitMatrix: number[][]
+    ) => SvgProps['children'];
   }
 
   const QRCodeStyled: ComponentType<QRCodeStyledProps>;
--- src/components/QRCodeCustom.tsx
+++ src/components/QRCodeCustom.tsx
@@ -14,19 +14,65 @@ const QRCodeCustom: React.FC<QRCodeCustomProps> = ({ value, size }) => {
         data={value}
         style={{ backgroundColor: 'white' }}
         padding={10}
-        pieceSize={5}
+        pieceSize={10}
+        pieceBorderRadius={2}
+        pieceStroke=''
         errorCorrectionLevel="H"
         color="black"
-        logo={{
-          uri: 'https://example.com/my-logo.png', // Remplacez par votre URL de logo
-          width: 40,
-          height: 40,
-        }}
         // size={size} // Utilisez la taille définie en props
       />
     </View>
   );
 };
+
+export function CirclePieces() {
+  return (
+    <QRCodeStyled
+      data={'Styling Pieces'}
+      style={styles.svg1}
+      padding={25}
+      pieceSize={8}
+      pieceBorderRadius={4}
+      color={'#F57F17'}
+    />
+  );
+}
+
+export function CustomEyes() {
+  return (
+    <QRCodeStyled
+      data={'Custom Corners'}
+      style={styles.svg2}
+      padding={20}
+      pieceSize={8}
+      pieceBorderRadius={4}
+      gradient={{
+        type: 'radial',
+        options: {
+          center: [0.5, 0.5],
+          radius: [1, 1],
+          colors: ['#ff7bc6', '#0f0080'],
+          locations: [0, 1],
+        },
+      }}
+      outerEyesOptions={{
+        topLeft: {
+          borderRadius: [20, 20, 0, 20],
+        },
+        topRight: {
+          borderRadius: [20, 20, 20],
+        },
+        bottomLeft: {
+          borderRadius: [20, 0, 20, 20],
+        },
+      }}
+      innerEyesOptions={{
+        borderRadius: 12,
+        scale: 0.85,
+      }}
+    />
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -34,6 +80,18 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  
+  svg1: {
+    backgroundColor: 'white',
+    borderRadius: 16,
+    overflow: 'hidden',
+  },
+
+  svg2: {
+    backgroundColor: 'white',
+    borderRadius: 36,
+    overflow: 'hidden',
+  },
 });
 
-export default QRCodeCustom;
+export default CustomEyes;
